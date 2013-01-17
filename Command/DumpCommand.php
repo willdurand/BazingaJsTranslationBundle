4fc006ee5c3fefc9f9b4eccaa0f57c3dd3ff9dec
Adrien Russo <adrien.russo.qc@gmail.com>
2013-01-17 16:41:45 -0500

    Refactory Translation Dumper. (bazinga:expose:translation:dump --symlink targetDir)

--- Command/DumpCommand.php
+++ Command/DumpCommand.php
@@ -3,40 +3,53 @@
 namespace Bazinga\ExposeTranslationBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Output\Output;
 
 /**
+ * Command that places translation assets into a given directory.
+ *
  * @author Adrien Russo <adrien.russo.qc@gmail.com>
  */
 class DumpCommand extends ContainerAwareCommand
 {
     /**
-     * configure
-     *
-     * @return null
+     * @see Command
      */
     protected function configure()
     {
         $this
             ->setName('bazinga:expose:translation:dump')
-            ->setDescription('Dumps all translation files');
+            ->setDefinition(array(
+                new InputArgument('target', InputArgument::OPTIONAL, 'The target directory', 'web'),
+            ))
+            ->addOption('symlink', null, InputOption::VALUE_NONE, 'Symlinks the assets instead of copying it')
+            ->setDescription('Dumps all translation files into a given directory.');
     }
 
     /**
-     * execute
+     * @see Command
      *
-     * @param InputInterface  $input  The input interface
-     * @param OutputInterface $output The output interface
-     *
-     * @return null
+     * @throws \InvalidArgumentException When the target directory does not exist
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $targetArg = rtrim($input->getArgument('target'), '/');
+
+        if (!is_dir($targetArg)) {
+            throw new \InvalidArgumentException(sprintf('The target directory "%s" does not exist.', $input->getArgument('target')));
+        }
+
+        if (!function_exists('symlink') && $input->getOption('symlink')) {
+            throw new \InvalidArgumentException('The symlink() function is not available on your system. You need to install the assets without the --symlink option.');
+        }
+
         $this
             ->getContainer()
             ->get("bazinga.exposetranslation.dumper.translation_dumper")
-            ->dump();
+            ->dump($targetArg, $input->getOption('symlink'));
     }
 }
