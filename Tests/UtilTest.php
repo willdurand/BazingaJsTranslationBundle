40147a0fd8a7116956a7255a0363d32aa2f8b89a
Hugo Alliaume <kocal@live.fr>
2020-08-06 21:19:27 +0200

    imp(util): make extractCatalogueInformationFromFilename return a 4th key: the format

--- Tests/UtilTest.php
+++ Tests/UtilTest.php
@@ -13,17 +13,18 @@ class UtilTest extends TestCase
      */
     public function testExtractCatalogueInformationFromFilename(string $filename, array $expectedInformation): void
     {
-        [$domain, $locale, $extension] = Util::extractCatalogueInformationFromFilename($filename);
+        [$domain, $locale, $extension, $format] = Util::extractCatalogueInformationFromFilename($filename);
 
         $this->assertSame($expectedInformation[0], $domain);
         $this->assertSame($expectedInformation[1], $locale);
         $this->assertSame($expectedInformation[2], $extension);
+        $this->assertSame($expectedInformation[3], $format);
     }
 
     public function provideExtractCatalogueInformationFromFilename(): iterable
     {
-        yield ['messages.en.yml', ['messages', 'en', 'yml']];
-        yield ['messages.en.xliff', ['messages', 'en', 'xliff']];
-        yield 'with ICU support' => ['messages+intl-icu.en.xliff', ['messages', 'en', 'xliff']];
+        yield ['messages.en.yml', ['messages', 'en', 'yml', null]];
+        yield ['messages.en.xliff', ['messages', 'en', 'xliff', null]];
+        yield 'with ICU support' => ['messages+intl-icu.en.xliff', ['messages', 'en', 'xliff', 'intl-icu']];
     }
 }
--- Util.php
+++ Util.php
@@ -12,13 +12,14 @@ class Util
     public static function extractCatalogueInformationFromFilename(string $filename)
     {
         [$domain, $locale, $extension] = explode('.', basename($filename), 3);
+        $format = null;
 
         // Domains using ICU Message Format suffix their names with "+intl-icu".
         // See https://symfony.com/doc/current/translation/message_format.html#using-the-icu-message-format
-        if (false !== $icuPos = strpos($domain, '+intl-icu')) {
-            $domain = substr($domain, 0, $icuPos);
+        if (false !== strpos($domain, '+')) {
+            [$domain, $format] = explode('+', $domain, 2);
         }
 
-        return array($domain, $locale, $extension);
+        return array($domain, $locale, $extension, $format);
     }
 }
