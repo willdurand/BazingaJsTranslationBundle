4c601be826a7c2065d280bc847a5594abeab0f55
Christian Vermeulen <info@christianvermeulen.net>
2020-12-08 10:39:07 +0100

    Check the commonJS before AMD

--- Resources/js/translator.js
+++ Resources/js/translator.js
@@ -3,14 +3,14 @@
  * @license MIT Licensed
  */
 (function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define(['IntlMessageFormat'], factory);
-    }
-    else if (typeof module === 'object' && module.exports) {
+    if (typeof module === 'object' && module.exports) {
         // Node. Does not work with strict CommonJS, but
         // only CommonJS-like environments that support module.exports,
         // like Node.
         module.exports = factory(require('intl-messageformat'));
+    }
+    else if (typeof define === 'function' && define.amd) {
+        define(['IntlMessageFormat'], factory);
     }
     else {
         root.Translator = factory(root.IntlMessageFormat);
