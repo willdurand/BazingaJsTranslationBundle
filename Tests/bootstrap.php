38f578f8d0c81496c9702a0655becd2a09401d05
Hugo Monteiro <hugo.monteiro@gmail.com>
2020-12-26 10:56:25 +0000

    fix

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -13,8 +13,4 @@ EOT;
     exit(1);
 }
 
-if (version_compare(PHP_VERSION, '8.0.0', '<')) {
-    libxml_disable_entity_loader(true);
-}
-
 $loader->add('Bazinga\Bundle\JsTranslationBundle\Tests', __DIR__);
